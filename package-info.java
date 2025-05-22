96b6f918a16757d2ccec5af56536b1ca87d67d20
Zak Goneau <zak.goneau@stu.ocsb.ca>
2025-05-22 22:58:42 +0000

    Added package info.

---
+++ package-info.java
@@ -0,0 +1,11 @@
+/** .
+* This is the package info for Main.java
+*
+* This program creates a point class and object, from there the
+* distance from the origin is calculated. Using inheritance and getting user input,
+* another point instance can be created and the distance between the two points is then calculated.
+*
+* @author  Zak Goneau
+* @version 1.0
+* @since   2025-05-22
+*/
